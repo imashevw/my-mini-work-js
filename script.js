77c68f30e180e9af2aff99111ad19638eb952961
bekzat <imashevbekzat77@gmail.com>
2023-02-26 10:17:59 +0300

    we

---
+++ script.js
@@ -0,0 +1,6 @@
+let buttonBtn = document.querySelector("button")
+buttonBtn.addEventListener("click", function(){
+        alert("Hello")
+        alert("bye")
+        alert("your very beautiful")
+});
